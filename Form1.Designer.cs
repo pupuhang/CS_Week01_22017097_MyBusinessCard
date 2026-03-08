8fb03808cb3c78823014bf14829c32ddef27c43e
pupuhang <zp2558@gmail.com>
2026-03-08 16:07:18 +0900

    추가 기능: 깃허브 주소 클릭시 깃허브로 이

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,18 +34,23 @@
             label2 = new Label();
             label3 = new Label();
             button1 = new Button();
+            label4 = new Label();
+            pictureBox2 = new PictureBox();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(64, 79);
+            pictureBox1.Location = new Point(99, 79);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(152, 201);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // label1
             // 
@@ -74,7 +79,7 @@
             label3.AutoSize = true;
             label3.Font = new Font("함초롬돋움", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.FromArgb(64, 64, 64);
-            label3.Location = new Point(321, 200);
+            label3.Location = new Point(321, 165);
             label3.Name = "label3";
             label3.Size = new Size(202, 27);
             label3.TabIndex = 3;
@@ -85,7 +90,7 @@
             button1.BackColor = Color.Gray;
             button1.Font = new Font("함초롬바탕", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
             button1.ForeColor = Color.White;
-            button1.Location = new Point(581, 351);
+            button1.Location = new Point(558, 332);
             button1.Name = "button1";
             button1.Size = new Size(129, 41);
             button1.TabIndex = 4;
@@ -93,20 +98,59 @@
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("맑은 고딕", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.Highlight;
+            label4.Location = new Point(356, 246);
+            label4.Name = "label4";
+            label4.Size = new Size(240, 21);
+            label4.TabIndex = 5;
+            label4.Text = "https://github.com/pupuhang";
+            label4.Click += label4_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(321, 238);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(29, 29);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(135, 286);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 7;
+            button2.Text = "사진 변경";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(pictureBox2);
+            Controls.Add(label4);
             Controls.Add(button1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
-            ForeColor = Color.Transparent;
+            ForeColor = Color.Black;
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -118,5 +162,8 @@
         private Label label2;
         private Label label3;
         private Button button1;
+        private Label label4;
+        private PictureBox pictureBox2;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -13,5 +13,25 @@ namespace MyBusinessCard
             //R, G, B 값을 무작위로 생성, 배경색에 적용
             this.BackColor = Color.FromArgb(rd.Next(256), rd.Next(256), rd.Next(255));
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer", "https://github.com/pupuhang");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer", "https://github.com/pupuhang");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.new_pic;
+        }
     }
 }
