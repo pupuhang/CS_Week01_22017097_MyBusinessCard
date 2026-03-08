f9ff4300b9ec591dd716f6de4687236a76f75e28
pupuhang <zp2558@gmail.com>
2026-03-08 17:02:33 +0900

    코드 설명 추가

--- Form1.cs
+++ Form1.cs
@@ -17,31 +17,32 @@ namespace MyBusinessCard
         private void label4_Click(object sender, EventArgs e)
         {
             System.Diagnostics.Process.Start("explorer", "https://github.com/pupuhang");
+            //링크 클릭시 깃허브 페이지로 이동
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
             System.Diagnostics.Process.Start("explorer", "https://github.com/pupuhang");
+            //아이콘 클릭시 깃허브 페이지로 이동
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void button2_Click(object sender, EventArgs e)
         {
             pictureBox1.Image = Properties.Resources.new_pic;
+            //버튼 클릭시 이미지 변경
         }
 
         private void pictureBox1_MouseEnter(object sender, EventArgs e)
         {
             pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            //마우스가 이미지에 들어올 때 테두리 추가
         }
 
         private void pictureBox1_MouseLeave(object sender, EventArgs e)
         {
             pictureBox1.BorderStyle = BorderStyle.None;
+            //마우스가 이미지에서 나갈 때 테두리 제거
         }
     }
 }
